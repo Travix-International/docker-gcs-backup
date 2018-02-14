fa606868e2385c9bf669c8e675f40949f98044b1
Edsger de Looff <edelooff@travix.com>
2018-02-14 16:04:03 +0100

    change startupscript

--- start.sh
+++ start.sh
@@ -5,17 +5,8 @@ GCSBucket="$Store_Bucket"
 
 if [ -e "$GCSAuthFile" ]
 then
-  # mount google storage
   mkdir /mnt/gcs-store
-#  mount -t gcsfuse "$GCSBucket" /mnt/gcs-store
-#  setenforce 0
-#  sed -i s@.*user_allow_other@user_allow_other@g /etc/fuse.conf
   mount -t gcsfuse "$GCSBucket" /mnt/gcs-store -o dir_mode=777,file_mode=666,allow_other,user
-  smbd -F -S
-#else
-#  # create auth file
-#  /opt/google-cloud-sdk/bin/gcloud auth login
+  exec smbd -FS < /dev/null
 fi
 
-# last line!
-#/bin/bash
