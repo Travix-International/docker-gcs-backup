ddf2b3c6c867d07aebac85453d5c81534377c480
Edsger de Looff <edelooff@travix.com>
2018-02-14 15:43:55 +0100

    don't put smbd in background

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ then
 #  setenforce 0
 #  sed -i s@.*user_allow_other@user_allow_other@g /etc/fuse.conf
   mount -t gcsfuse "$GCSBucket" /mnt/gcs-store -o dir_mode=777,file_mode=666,allow_other,user
-  smbd -F -S &
+  smbd -F -S
 #else
 #  # create auth file
 #  /opt/google-cloud-sdk/bin/gcloud auth login
 fi
 
 # last line!
-/bin/bash
+#/bin/bash
